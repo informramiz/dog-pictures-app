80d1616df437b8ebabefa3ba925d7df08d30669c
Ramiz Raja <informramiz@gmail.com>
2019-07-12 20:02:54 +0500

    chore: Create project

---
+++ DogApp/DogApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogApp
+//
+//  Created by Ramiz Raja on 12/07/2019.
+//  Copyright © 2019 Ramiz Raja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
