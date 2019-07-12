ca8c6cbee727c9d0c583b7a2ddaa6382aa4242b3
Ramiz Raja <informramiz@gmail.com>
2019-07-12 20:10:43 +0500

    feat: Add DogImage model

---
+++ DogApp/DogApp/Model/DogImage.swift
@@ -0,0 +1,14 @@
+//
+//  DogImage.swift
+//  DogApp
+//
+//  Created by Ramiz Raja on 12/07/2019.
+//  Copyright © 2019 Ramiz Raja. All rights reserved.
+//
+
+import Foundation
+
+struct DogImage: Codable {
+    let message: String
+    let status: String
+}
