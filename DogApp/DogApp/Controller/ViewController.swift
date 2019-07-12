61b52d2a128a907160f2ab28685fc7d70bd6ba3a
Ramiz Raja <informramiz@gmail.com>
2019-07-13 00:29:24 +0500

    feat: Add picker view outlet

--- DogApp/DogApp/Controller/ViewController.swift
+++ DogApp/DogApp/Controller/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
