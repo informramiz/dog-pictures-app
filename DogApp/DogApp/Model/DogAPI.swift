376f8dd06c75fc24949a15aca7324ca4771b18c9
Ramiz Raja <informramiz@gmail.com>
2019-07-12 20:07:50 +0500

    feat: Add basic DogAPI class

---
+++ DogApp/DogApp/Model/DogAPI.swift
@@ -0,0 +1,15 @@
+//
+//  DogAPI.swift
+//  DogApp
+//
+//  Created by Ramiz Raja on 12/07/2019.
+//  Copyright © 2019 Ramiz Raja. All rights reserved.
+//
+
+import Foundation
+
+class DogAPI {
+    enum EndPoint: String {
+        case randomImageFromAllDogs = "https://dog.ceo/api/breeds/image/random"
+    }
+}
